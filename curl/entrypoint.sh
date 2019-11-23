1196ad2b760f9b69334fdb7dd0448a96442c9ffe
Lawrence Cherone <lawrence@cherone.co.uk>
2019-11-23 10:32:38 +0000

    .

--- curl/entrypoint.sh
+++ curl/entrypoint.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
+set -e
+
 # debug
 echo >&2 "Entrypoint: [ENV]"
 echo >&2 "$(printenv)"
 
 echo >&2 "Executing: curl $INPUT_ARGS"
+
+sh -c "/usr/bin/curl $*"
 
 /usr/bin/curl $INPUT_ARGS
 
