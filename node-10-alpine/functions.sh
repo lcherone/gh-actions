4715f6adcf8a3feb153457cf39680f069a709c19
Lawrence Cherone <lawrence@cherone.co.uk>
2019-11-23 03:00:57 +0000

    .

--- node-10-alpine/functions.sh
+++ node-10-alpine/functions.sh
@@ -1,21 +1,21 @@
 #
 function help {
-  echo "The following commands exist:"
-  echo ""
-  echo "  - run <script>"
-  echo "      execute the specified <script> on the package"
-  echo "  - test"
-  echo "      execute the test script"
-  echo "  - build"
-  echo "      execute the build script"
-  echo ""
+  echo >&2 "The following commands exist:"
+  echo >&2 ""
+  echo >&2 "  - run <script>"
+  echo >&2 "      execute the specified <script> on the package"
+  echo >&2 "  - test"
+  echo >&2 "      execute the test script"
+  echo >&2 "  - build"
+  echo >&2 "      execute the build script"
+  echo >&2 ""
 }
 
 # This function will process the provided script with either
 # NPM or Yarn
 #   usage: runCommand [npm|yarn] <script>
 function runCommand {
-  echo "runCommand script: $1" 
+  echo >&2 "runCommand script: $1" 
   script="$1"
 
   if [[ "$PKG_MANAGER" =~ ^[Yy]arn$ ]]; then
@@ -23,21 +23,21 @@ function runCommand {
   fi
 
   if [[ -z "$script" ]]; then
-    echo "[ERROR] No script provided to execute"
+    echo >&2 "[ERROR] No script provided to execute"
     exit 1
   fi
 
   case $PKG_MANAGER in
     yarn)
-      echo "Running: yarn $script"
-      npm install yarn -g > /dev/null 2>&1
+      echo >&2 "Running: yarn $script"
+      npm install yarn -g #> /dev/null 2>&1
       yarn install > /dev/null 2>&1
       yarn $script
       ;;
 
     *)
-      echo "Running: npm run $script"
-      npm install > /dev/null 2>&1
+      echo >&2 "Running: npm run $script"
+      npm install #> /dev/null 2>&1
       npm run $script
       ;;
   esac
