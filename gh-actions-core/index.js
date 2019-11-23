fe9aabafa7af8aa0fbc593077c17ca560f8e8d54
Lawrence Cherone <lawrence@cherone.co.uk>
2019-11-23 05:30:48 +0000

    .

--- gh-actions-core/index.js
+++ gh-actions-core/index.js
@@ -1,11 +1,55 @@
+/* see: https://github.com/actions/toolkit */
 const core = require('@actions/core')
 const github = require('@actions/github')
+const exec = require('@actions/exec')
+const io = require('@actions/io')
+const tc = require('@actions/tool-cache')
 
 async function main() {
   try {
-    console.log('process.env', process.env)
-    console.log('github.context', github.context)
+    /**
+     * @actions/core examples
+     */
+    core.debug('Inside try block');
+
+    // Manually wrap output's
+    // - process.env
+    core.startGroup('Environment')
+    console.log(process.env)
+    core.endGroup()
+    // - github.context
+    core.startGroup('Github context')
+    console.log(github.context)
+    core.endGroup()
+
+    // Manually wrap output's
+    await core.group('Do something async', async () => {
+      return Promise.resolve('noice!')
+    })
+
+    /**
+     * @actions/github examples
+     */
+    // This should be a token with access to your repository scoped in as a secret.
+    // The YML workflow will need to set myToken with the GitHub Secret Token
+    // myToken: ${{ secrets.GITHUB_TOKEN }}
+    // https://help.github.com/en/articles/virtual-environments-for-github-actions#github_token-secret
+    /*
+    const myToken = core.getInput('myToken');
+
+    //const octokit = new github.GitHub(myToken);
+    octokit.log.info('Octokit woop!')
+    */
+
+    /**
+     * @actions/exec example
+     * @actions/io example
+     */
+    const npmPath = await io.which('npm', true)
+    await exec.exec(`"${npmPath}"`, ['run', 'test'])
+
   } catch (error) {
+    core.error(`Error: ${error.message}`);
     core.setFailed(error.message)
   }
 }
