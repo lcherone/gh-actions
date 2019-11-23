f03b7cd49b5480b87c5b7ab9c9f9c61764911198
Lawrence Cherone <lawrence@cherone.co.uk>
2019-11-23 10:00:18 +0000

    .

--- curl/entrypoint.sh
+++ curl/entrypoint.sh
@@ -5,4 +5,7 @@ echo >&2 "Entrypoint: [ENV]"
 echo >&2 "$(printenv)"
 
 echo >&2 "Executing: curl -s $INPUT_ARGS"
-echo ::set-output name=stdout::$(curl -s $INPUT_ARGS)
+
+output=$(curl -s $INPUT_ARGS)
+
+echo ::set-output name=stdout::"$output"
