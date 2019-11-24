b4c3118512b126f74519a63c5171283fdcd0ef15
Lawrence Cherone <lawrence@cherone.co.uk>
2019-11-24 10:41:54 +0000

    .

--- lxd/index.js
+++ lxd/index.js
@@ -8,7 +8,6 @@ const fs = require('fs')
 const setupScript = {
   lxd: `
 #!/bin/bash
-echo >&2 "$(printenv)"
 export PATH=./node_modules/.bin:/usr/bin:/bin:/snap/bin:$PATH
 set -eo pipefail
 sudo apt-get -y remove lxd lxd-client
@@ -17,7 +16,7 @@ sudo apt-get --yes install snapd
 sudo snap install lxd
 sudo ln -s /snap/bin/lxc /usr/bin/lxc
 sudo lxd waitready
-sudo lxd init --auto --network-address="127.0.0.1" --storage-backend=dir --trust-password=$LXD_SECRET
+sudo lxd init --auto --network-address="127.0.0.1" --storage-backend=dir #--trust-password=$LXD_SECRET
 openssl genrsa 2048 > client.key
 openssl req -new -x509 -nodes -sha1 -days 365 -key client.key -out client.crt -subj "/C=GB/ST=London/L=London/O=TEST/OU=IT Department/CN=lxd.localhost"
 sudo lxd.lxc config trust add client.crt
@@ -54,19 +53,70 @@ async function main() {
     /**
      * LXD
      */
+    let input = {
+      remote: (function () {
+        let remote = core.getInput('remote')
+        if (remote !== '{}') {
+          try {
+            return JSON.parse(remote)
+          } catch (_) {
+            core.setFailed(`Error: Invalid JSON input: remove`)
+          }
+        }
+        return {
+          name: core.getInput('remote-name') || '',
+          url: core.getInput('remote-url') || '',
+          secret: core.getInput('remote-secret') || '',
+          protocol: core.getInput('remote-protocol') || 'lxd',
+          authType: core.getInput('remote-auth-type') || 'tls'
+        }
+      })(),
+      command: core.getInput('command')
+    }
+
+    core.startGroup('inputs')
+    console.log(input)
+    core.endGroup()
+
     let result
 
     //
     lxc.setCmd('sudo lxd.lxc')
 
-    core.startGroup('local LXD server: lxc remote list: before')
-    try {
-      result = await lxc.local('lxc remote list')
-      console.log('result', result)
-    } catch (err) {
-      console.log('err', err)
+    // add remote
+    if (input.remote.name !== '' && input.remote.url !== '' && input.remote.secret !== '') {
+      core.startGroup('adding remote to LXD server: ' + input.command)
+      let remotes = await lxc.server.remotes()
+      //
+      if (!remotes.incudes(input.remote.name)) {
+        try {
+          result = await lxc.local(
+            `lxc remote add ${input.remote.name} ${input.remote.url} ` +
+            `--accept-certificate ` +
+            `--protocol=${input.remote.protocol} ` +
+            `--auth-type=${input.remote.authType} ` +
+            `--password=${input.remote.secret}`, {}, false)
+          core.info(result)
+        } catch (err) {
+          core.error(err)
+        }
+      } else {
+        core.info('Skipping, remote already added')
+      }
+      core.endGroup()
     }
-    core.endGroup()
+
+    // command
+    if (input.command !== '') {
+      core.startGroup('local LXD server: ' + input.command)
+      try {
+        result = await lxc.local(input.command, {}, false)
+        core.info(result)
+      } catch (err) {
+        core.error(err)
+      }
+      core.endGroup()
+    }
 
     // //
     // const script = core.getInput('script')
